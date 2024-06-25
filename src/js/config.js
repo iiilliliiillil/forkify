f8c703b17389c9cd901a593a476a1417bb63ce68
Yusifov Rustam <noirtxt@gmail.com>
2024-06-25 19:15:51 +0400

    api

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '<YOUR_KEY>';
+export const KEY = 'aab87722-cd10-4e98-8ffd-b477ee6c9ba0';
 export const MODAL_CLOSE_SEC = 2.5;
