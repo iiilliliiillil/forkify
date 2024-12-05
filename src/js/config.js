0ae62d70522c1b91705b1c9d1e441603a7db08a7
Rusik <63919899+iiilliliiillil@users.noreply.github.com>
2024-12-05 16:54:29 +0400

    Update config.js from server

--- src/js/config.js
+++ src/js/config.js
@@ -3,3 +3,4 @@ export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
 export const KEY = 'aab87722-cd10-4e98-8ffd-b477ee6c9ba0';
 export const MODAL_CLOSE_SEC = 2.5;
+console.log('i changed from github')
